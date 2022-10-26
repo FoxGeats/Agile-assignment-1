af968f99abc254476cfeeb30b9a0c65cf1107030
Shu Chen <GGGGeats@gmail.com>
2022-10-26 16:59:13 +0100

    Initial commit

---
+++ src/pages/upcomingMoviesPage.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovieUpcoming } from "../api/tmdb-api";
+const HomePage = (props) => {
+  const [movies, setMovies] = useState([]);
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+  useEffect(() => {
+    getMovieUpcoming().then(movies => {
+      setMovies(movies);
+    });
+  }, []);
+
+  return (
+    <PageTemplate
+      title='Upcoming Movies'
+      movies={movies}
+      selectFavorite={addToFavorites}
+    />
+  );
+};
+export default HomePage;
+
+
+/*
+import React, { useContext } from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { MoviesContext } from "../contexts/moviesContext";
+import { useQueries } from "react-query";
+import { getMovie } from "../api/tmdb-api";
+import Spinner from '../components/spinner'
+import RemoveFromFavorites from "../components/cardIcons/removeFromFavorites";
+import WriteReview from "../components/cardIcons/writeReview";
+
+const FavoriteMoviesPage = () => {
+  const {favorites: movieIds } = useContext(MoviesContext);
+
+  // Create an array of queries and run in parallel.
+  const favoriteMovieQueries = useQueries(
+    movieIds.map((movieId) => {
+      return {
+        queryKey: ["movie", { id: movieId }],
+        queryFn: getMovie,
+      };
+    })
+  );
+  // Check if any of the parallel queries is still loading.
+  const isLoading = favoriteMovieQueries.find((m) => m.isLoading === true);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const movies = favoriteMovieQueries.map((q) => {
+    q.data.genre_ids = q.data.genres.map(g => g.id)
+    return q.data
+  });
+
+  const toDo = () => true;
+
+  return (
+    <PageTemplate
+      title="Favorite Movies"
+      movies={movies}
+      action={(movie) => {
+        return (
+          <>
+            <RemoveFromFavorites movie={movie} />
+            <WriteReview movie={movie} />
+          </>
+        );
+      }}
+    />
+  );
+};
+
+export default FavoriteMoviesPage;
+*/
